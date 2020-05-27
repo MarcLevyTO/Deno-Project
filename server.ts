2ce6fbdf6d5b7ea724600696f9be0f6393fb38de
Marc Levy <marc.levy.to@me.com>
2020-05-27 16:00:57 -0400

    added DENON and DENON file

--- server.ts
+++ server.ts
@@ -3,12 +3,11 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./routes.ts";
 
 const port = 5001;
-
 const app = new Application();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`Server running on port ${port}`);
+console.log(`Server running on port ${port}`, Deno.env.get("TOKEN"));
 
 await app.listen({ port });
